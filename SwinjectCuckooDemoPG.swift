1e637a301b931b1949d0087b57afc1a1e2e2e907
Ranxan <ranxan4ix@gmail.com>
2017-05-08 16:50:18 +0545

    Update SwinjectCuckooDemoPG.swift

--- SwinjectCuckooDemoPG.swift
+++ SwinjectCuckooDemoPG.swift
@@ -1,3 +1,39 @@
+
+/**Pods*/
+/*********************************************************************************************************/
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwinjectCuckooProject' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwinjectCuckooProject
+
+    # DI framework
+    pod 'Swinject',   '~> 2.1.0'
+
+  target 'SwinjectCuckooProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+    
+    # Mocking
+    pod 'Cuckoo'
+    
+  end
+
+  target 'SwinjectCuckooProjectUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
+
+
+
+
+
 //: Playground - noun: a place where people can play
 
 import UIKit
